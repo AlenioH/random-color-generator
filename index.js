bb5afcbd01c3e627c94f5963dd7d4527a02e8c9a
Alena Hasslacher <alena.hasslacher@gmail.com>
2020-05-08 21:17:39 +0200

    Debug + improve
    There was a bug in the conditional in the previous version, in this one
    I chose to remove the conditional completely.

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
 let randomColor = require('randomcolor');
-let color = randomColor();
-
 let createColor = require('my-colors');
-
-let userColor = process.argv[2];
 let toHex = require('colornames');
-let userColHex = toHex(userColor);
 
-let luminosity = process.argv[3];
+let color = randomColor({
+  hue: process.argv[2],
+  luminosity: process.argv[3],
+});
 
-if (userColor && luminosity === '') {
-  let colorSchemeUser = createColor(userColor, 'black', 'bold');
-  let finalMessageUser = colorSchemeUser(`##############################
-  ##############################
-  ###########         ##########
-  ########### ${userColHex} ##########
-  ###########         ##########
-  ##############################
-  ##############################`);
-  console.log(finalMessageUser);
-} else if (luminosity) {
-  let colorHex = randomColor({
-    hue: process.argv[2],
-    luminosity: process.argv[3],
-  });
-  let colorSchemeUserLu = createColor(colorHex, 'black', 'bold');
-  let finalMessageUserLu = colorSchemeUserLu(`##############################
-  ##############################
-  ###########         ##########
-  ########### ${userColHex} ##########
-  ###########         ##########
-  ##############################
-  ##############################`);
-  console.log(finalMessageUserLu);
-} else {
-  let colorSchemeRandom = createColor(color, 'black', 'bold');
-  let finalMessageRandom = colorSchemeRandom(`##############################
+let colorScheme = createColor(color, 'black', 'bold');
+let finalMessage = colorScheme(`##############################
 ##############################
 ###########         ##########
 ########### ${color} ##########
@@ -43,5 +16,4 @@ if (userColor && luminosity === '') {
 ##############################
 ##############################`);
 
-  console.log(finalMessageRandom);
-}
+console.log(finalMessage);
