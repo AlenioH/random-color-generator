cfa493b887665327a75d8edaad3351f459a0d737
Alena Hasslacher <alena.hasslacher@gmail.com>
2020-05-16 17:29:17 +0200

    Fix bug with black background, remove unused libs

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 let randomColor = require('randomcolor');
 let createColor = require('my-colors');
-let toHex = require('colornames');
 
 let color = randomColor({
   hue: process.argv[2],
   luminosity: process.argv[3],
 });
 
-let colorScheme = createColor(color, 'black', 'bold');
+let colorScheme = createColor(color, 'ybold');
 let finalMessage = colorScheme(`##############################
 ##############################
 ###########         ##########
