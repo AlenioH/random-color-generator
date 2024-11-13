900811cb3977d1afb0d17486892e569c1e4245d4
Alena Hasslacher <aha@MacBook-Pro.fritz.box>
2024-11-13 12:43:46 +0100

    Add error handling, replace let with const, update readme

--- README.md
+++ README.md
@@ -4,4 +4,9 @@ The task was to create a CLI application that would return a random color when r
 
 ### What it does
 
-It does return a hash block in a random color with the hex code of that color when is run in command line. If user specifies a color (e.g. "red"), the block will be colored that and return the hex code of the color chosen. If luminosity is added to that (e.g. "red dark"), the block is returned in this color.
+It returns a hash block in a random color with the hex code of that color when run in command line. If user specifies a color (e.g. "red"), the block will be colored that and return the hex code of the color chosen. Currently supported colors by the library are: red, orange, yellow, green, blue, purple, pink and monochrome. If luminosity is added to that (e.g. "red dark"), the block is returned in this color. Supported values for luminosity are: dark, light and bright. 
+
+To run the application in the command line use for example:
+```
+node index.js red light
+```
--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-let randomColor = require('randomcolor');
-let createColor = require('my-colors');
+const randomColor = require('randomcolor');
+const createColor = require('my-colors');
 
-let color = randomColor({
-  hue: process.argv[2],
-  luminosity: process.argv[3],
+const SUPPORTED_HUES = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome'];
+const SUPPORTED_LUMINOSITIES = ['bright', 'light', 'dark'];
+
+const hue = process.argv[2];
+const luminosity = process.argv[3];
+
+if (hue && !SUPPORTED_HUES.includes(hue)) {
+  console.log("Error: Unsupported hue provided.");
+  console.log("Supported hues are:", SUPPORTED_HUES.join(', '));
+  return;
+}
+
+if (luminosity && !SUPPORTED_LUMINOSITIES.includes(luminosity)) {
+  console.log("Error: Unsupported luminosity provided.");
+  console.log("Supported luminosities are:", SUPPORTED_LUMINOSITIES.join(', '));
+  return;
+}
+
+const color = randomColor({
+  hue: hue || 'random',
+  luminosity: luminosity || 'random',
+  format: 'hex',
 });
 
-let colorScheme = createColor(color, 'bold');
-let finalMessage = colorScheme(`##############################
+const colorScheme = createColor(color, 'bold');
+const finalMessage = colorScheme(`##############################
 ##############################
 ###########         ##########
 ########### ${color} ##########
